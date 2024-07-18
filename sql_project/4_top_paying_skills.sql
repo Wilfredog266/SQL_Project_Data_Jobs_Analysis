413cb13dd3a513f333923ea0a9ed9021d5224b5b
Wilfredog266 <wilfredog266@gmail.com>
2024-07-18 19:47:36 -0400

    fix

--- sql_project/4_top_paying_skills.sql
+++ sql_project/4_top_paying_skills.sql
@@ -1,7 +1,7 @@
 /*
 What are the top skills based on salary?
 - Look at the average salary associated with each skill for Data Analyst positions
-- Focuses on roles with specified salaries (NO NULLS), regardless of location (Remote jobs)
+- Focuses on roles with specified salaries (NO NULLS), regardless of location.
 - Why? It reveals how different skills impact salary levels for Data Analysts and 
     helps identify the most financially rewarding skills to acquire or improve on 
 */
